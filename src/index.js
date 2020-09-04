61fe6f1d38e0f2e02d65e0bfce253fd180a1765e
duynguyen158 <57234880+duynguyen158@users.noreply.github.com>
2020-09-04 22:37:28 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React, {Component} from "react";
+import React, { useEffect, createRef } from "react";
+import ReactDOM from "react-dom";
+import {Runtime, Inspector} from "@observablehq/runtime";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import define from "@coindesk-research/localbitcoins-paxful-activity-map";
+
+function Container() {
+    const toDisplay = ["viewof platform", "viewof column", "viewof encoder", 
+                       "viewof month", "map", "legend", "update", "iUpdate"];
+    const refMap = new Map(toDisplay.map(c => [c, createRef()]));
+
+    useEffect(
+        () => {
+            const runtime = new Runtime();
+            runtime.module(define, name => {
+                if (refMap.get(name)) {
+                    return new Inspector(refMap.get(name).current);
+                }
+            });
+            return () => runtime.dispose();
+        },
+        [refMap]
+    );
+
+    return (
+        <>
+            {toDisplay.map((c, i) => <div ref={refMap.get(c)} key={i} id={c} />)}
+        </>
+    );
+}
+
+// If you're feeling vintage ;)
+/*class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        const displayed = ["viewof platform", "viewof column", "viewof encoder", 
+                           "viewof month", "map", "legend", "update", "iUpdate"];
+        this.displayed = displayed;
+        this.ref = new Map(displayed.map(c => [c, React.createRef()]));
+    }
+
+    componentDidMount() {
+        const runtime = new Runtime();
+        runtime.module(define, name => {
+            if (this.ref.get(name)) {
+                return new Inspector(this.ref.get(name).current);
+            }
+        });
+        return () => runtime.dispose();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.displayed.map(c => <div ref={this.ref.get(c)}></div>)}
+            </div>
+        );
+    }
+}*/
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Container />,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
